994706904863cb7b43aca87414d612814b9b8f10
domidoma <65504542+DomiDoma@users.noreply.github.com>
2022-09-05 23:24:21 +0200

    autohide sidebar when needed, timetable bugfixes

--- neptune sexy mod.user.js
+++ neptune sexy mod.user.js
@@ -70,6 +70,19 @@ window.onload = function () {
             "#function_tableheader td.FunctionHeaderItem"
         ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 330 330" style="enable-background:new 0 0 330 330;" xml:space="preserve"><g id="XMLID_13_"><path id="XMLID_14_" d="M175.605,104.393c-2.814-2.813-6.628-4.393-10.607-4.393c-3.979,0-7.794,1.581-10.607,4.394l-79.998,80   c-5.857,5.858-5.857,15.355,0.001,21.213c5.857,5.857,15.355,5.858,21.213-0.001l69.392-69.393l69.395,69.394   C237.322,208.536,241.161,210,245,210s7.678-1.464,10.606-4.394c5.858-5.858,5.858-15.355,0-21.213L175.605,104.393z"/><path id="XMLID_15_" d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M165,300   c-74.439,0-135-60.561-135-135S90.561,30,165,30s135,60.561,135,135S239.439,300,165,300z"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>`;
 
+      
+        const hamBtn = document.createElement("li");
+      hamBtn.classList.add("ham-btn");
+      hamBtn.classList.add("menu-parent");
+      hamBtn.textContent="Sidemenu";
+      var isOpen=true;
+      hamBtn.addEventListener("click",()=>{
+        if(isOpen){document.querySelector(".left_boxes").classList.add("open-sidemenu");}
+        else{document.querySelector(".left_boxes").classList.remove("open-sidemenu");}
+        isOpen=!isOpen;
+      });
+        document.querySelector("#mb1.menubar").appendChild(hamBtn);
+      
         head.removeChild(customCss);
         head.appendChild(customCss);
     } catch {
